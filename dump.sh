efd81d93b7b6195bb567d7bf9ef287c8eda3709e
Long Nguyen <long.nguyen@logrhythm.com>
2021-03-26 00:16:07 -0600

    feat: add support for backup retention

--- dump.sh
+++ dump.sh
@@ -2,7 +2,6 @@
 
 # enable unofficial bash strict mode
 set -o errexit
-set -o nounset
 set -o pipefail
 IFS=$'\n\t'
 
@@ -38,10 +37,29 @@ function restic_backup {
     restic backup --tag "${PGHOST}" --tag "logical-backup" --stdin --stdin-filename "$BACKUP_FILENAME"
 }
 
+function delete_old_backups {
+  args=()
+
+  [[ -n "${BACKUP_RETENTION_KEEP_LAST:-}" ]] && args+=(--keep-last "$BACKUP_RETENTION_KEEP_LAST")
+  [[ -n "${BACKUP_RETENTION_KEEP_HOURLY:-}" ]] && args+=(--keep-hourly "$BACKUP_RETENTION_KEEP_HOURLY")
+  [[ -n "${BACKUP_RETENTION_KEEP_DAILY:-}" ]] && args+=(--keep-daily "$BACKUP_RETENTION_KEEP_DAILY")
+  [[ -n "${BACKUP_RETENTION_KEEP_WEEKLY:-}" ]] && args+=(--keep-weekly "$BACKUP_RETENTION_KEEP_WEEKLY")
+  [[ -n "${BACKUP_RETENTION_KEEP_MONTHLY:-}" ]] && args+=(--keep-monthly "$BACKUP_RETENTION_KEEP_MONTHLY")
+  [[ -n "${BACKUP_RETENTION_KEEP_YEARLY:-}" ]] && args+=(--keep-yearly "$BACKUP_RETENTION_KEEP_YEARLY")
+
+  if [[ "${#args[@]}" -gt "0" ]]; then
+    echo "Deleting old backups based on retention rule:" "${args[@]//\'/}"
+    restic forget --group-by tags --tag "${PGHOST}" "${args[@]//\'/}" --prune
+  else
+    echo "No retention rules detected. Skip deleting old backup"
+  fi
+}
+
 set -x
 initialize_restic_repo
 dump | restic_backup
 [[ ${PIPESTATUS[0]} != 0 || ${PIPESTATUS[1]} != 0 ]] && (( ERRORCOUNT += 1 ))
+delete_old_backups
 set +x
 
 exit "$ERRORCOUNT"
